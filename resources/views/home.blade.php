718ec34e859a638ac48ea0b47ce9025de71c6fd5
bhavok <bhavok.antil@gmail.com>
2020-06-07 20:19:28 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+        <img src="https://i.vsco.co/59c7248547a9d3382b6e8a76?size=120&c=1&dpr=1" class="rounded-circle">
+        
+        </div>
+    <div class="col-9 pt-5">
+    <div><h1>instaProject :)</h1></div>
+    <div class="d-flex">
+    <div class="pr-5"><strong>153</strong> posts</div>
+    <div class="pr-5"><strong>69k</strong> follwers</div>
+    <div class="pr-5"><strong>212</strong> following</div>
+    </div>
+    <div class="pt-4 font-weight-bold">vsco.com</div>
+    <div>This is photo sharing platform.Thanks</div>
+    </div>
+    <div class="row pt-5">
+    <div class="col-4 ">
+      <img src="https://instagram.fdel21-1.fna.fbcdn.net/v/t51.2885-15/e35/c114.0.454.454a/75244451_1366402563527809_8160879569007441350_n.jpg?_nc_ht=instagram.fdel21-1.fna.fbcdn.net&_nc_cat=1&oh=c34871e4c0a0cf3c1dfd7285acaddfed&oe=5E878758" alt="" class="w-100">
+      
+    </div>
+    <div class="col-4 ">
+      <img src="https://instagram.fdel21-1.fna.fbcdn.net/v/t51.2885-15/e35/c114.0.454.454a/75244451_1366402563527809_8160879569007441350_n.jpg?_nc_ht=instagram.fdel21-1.fna.fbcdn.net&_nc_cat=1&oh=c34871e4c0a0cf3c1dfd7285acaddfed&oe=5E878758" alt="" class="w-100">
+      
+    </div>
+    
+    <div class="col-4">
+      <img src="https://instagram.fdel21-1.fna.fbcdn.net/v/t51.2885-15/e35/c114.0.454.454a/75244451_1366402563527809_8160879569007441350_n.jpg?_nc_ht=instagram.fdel21-1.fna.fbcdn.net&_nc_cat=1&oh=c34871e4c0a0cf3c1dfd7285acaddfed&oe=5E878758" alt=""class="w-100">
+      
+    </div>
+    </div>
+</div>
+@endsection
